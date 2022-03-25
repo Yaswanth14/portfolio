08f91a37c4bafcc45fe252df64477c371dcece24
yaswanth14 <yaswanth14333@gmail.com>
2022-03-25 23:44:04 +0530

    Adding bootstrap

--- home/views.py
+++ home/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is my homepage (/)")
+    # return HttpResponse("This is my homepage (/)")
+    context = {'Name':'Yash', 'Course':'Django'}
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse("This is my aboutpage (/about)")
+    # return HttpResponse("This is my aboutpage (/about)")
+    return render(request, 'about.html')
 
 def projects(request):
-    return HttpResponse("This is my projectspage (/projects)")
+    # return HttpResponse("This is my projectspage (/projects)")
+    return render(request, 'projects.html')
 
 def contact(request):
-    return HttpResponse("This is my contactpage (/contact)")
+    # return HttpResponse("This is my contactpage (/contact)")
+    return render(request, 'contact.html')
