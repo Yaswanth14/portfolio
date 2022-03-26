b4f151998aebce75edbad1118d39459d12574d23
yaswanth14 <yaswanth14333@gmail.com>
2022-03-26 23:53:00 +0530

    models and admin support

--- home/models.py
+++ home/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    desc = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
 from django.shortcuts import render, HttpResponse
+from home.models import Contact 
 
 # Create your views here.
 def home(request):
@@ -17,6 +18,14 @@ def projects(request):
 
 def contact(request):
     if request.method == "POST":
-        print("This is a post")
+        
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        desc = request.POST['desc']
+        # print(name, email, phone, desc)
+        ins = Contact(name=name, email=email, phone=phone, desc=desc)
+        ins.save()
+        print("The data has been written to the db")
     # return HttpResponse("This is my contactpage (/contact)")
     return render(request, 'contact.html')
